3acd09cc0a447bfcd2785c94c90d16f2c24711c0
Luis Pacheco <ec2-user@ip-172-31-13-199.ec2.internal>
2026-05-18 14:33:04 +0000

    Correcao das vulnerabilidades

--- main.tf
+++ main.tf
@@ -10,6 +10,23 @@ resource "aws_vpc" "terraform_vpc" {
 }
 
 
+# Correcao primeira issue
+resource "aws_flow_log" "example" {
+  log_destination      = "arn:aws:s3:::clc15-pacheco-terraform"
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.terraform_vpc.id
+}
+
+# Correcao segunda issue
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.terraform_vpc.id
+  
+  tags = {
+    Name = "my-iac-sg"
+  }
+}
+
 #Cria as Subnets
 #Subnets A
 
